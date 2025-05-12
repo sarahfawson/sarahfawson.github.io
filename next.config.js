5f45a1a128cb7dfe45ae4231ae73ba3c89de7dae
Sarah Fawson <42820041+sarahfawson@users.noreply.github.com>
2025-05-12 09:49:44 -0600

    Update next.config.js
    removed incorrect base path and cargo image connection

--- next.config.js
+++ next.config.js
@@ -3,15 +3,8 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'freight.cargo.site',
-        pathname: '/**',
-      },
-    ],
   },
-  basePath: '/sarahfawson.github.io',
+  basePath: '',
 }
 
 module.exports = nextConfig 
